81290c3cb5c291317495a0beb944f01e9f331166
Clickery <garret_villanueva@dlsu.edu.ph>
2020-12-11 20:04:30 +0800

    Uploading Project folder

---
+++ MobileInputLessons/Assets/Scripts/TouchSample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class TouchSample : MonoBehaviour
+{
+    private void Update()
+    {
+        int touchCount = Input.touchCount;
+
+        if (touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            Debug.Log($"Got Touch {t.phase}");
+        }
+    }
+}
