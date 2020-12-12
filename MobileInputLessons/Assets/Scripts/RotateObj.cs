fdb26b6315f2df161da8c9081d68d002f7d7110d
Clickery <garret_villanueva@dlsu.edu.ph>
2020-12-13 05:11:42 +0800

    Added Camera Rotation & Prefabs in Main Menu Scene

---
+++ MobileInputLessons/Assets/Scripts/RotateObj.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObj : MonoBehaviour
+{
+    private float rotationSpeed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
