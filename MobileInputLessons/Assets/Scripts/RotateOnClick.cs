c840b0ab73e290caf2e16c698cc8c872d359f1a0
Clickery <garret_villanueva@dlsu.edu.ph>
2020-12-14 06:36:18 +0800

    Added UI Buttons to Rotate Camera

---
+++ MobileInputLessons/Assets/Scripts/RotateOnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnClick : MonoBehaviour
+{
+    public GameObject cameraGameObject;
+    private float rotateQuarterHalf = 90.0f;
+
+    public void rotateToTheLeft()
+    {
+        cameraGameObject.transform.Rotate(Vector3.up * rotateQuarterHalf);
+    }
+
+    public void rotateToTheRight()
+    {
+        cameraGameObject.transform.Rotate(Vector3.down * rotateQuarterHalf);
+    }
+}
