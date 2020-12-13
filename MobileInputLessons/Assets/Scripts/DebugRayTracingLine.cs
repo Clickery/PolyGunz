e1cae4bc5488328c741d927845de501c1ccd8702
Clickery <garret_villanueva@dlsu.edu.ph>
2020-12-14 06:55:37 +0800

    updated UI Buttons in My Level

--- MobileInputLessons/Assets/Scripts/DebugRayTracingLine.cs
+++ MobileInputLessons/Assets/Scripts/DebugRayTracingLine.cs
@@ -8,6 +8,6 @@ public class DebugRayTracingLine : MonoBehaviour
     void Update()
     {
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 100;
-        Debug.DrawRay(transform.position, forward, Color.green);
+        Debug.DrawRay(transform.position, forward, Color.red);
     }
 }
