466e75ac61572ba4ffe86174477c4ee4713d611a
Clickery <garret_villanueva@dlsu.edu.ph>
2020-12-14 05:29:20 +0800

    Deleted & Added Scripts

---
+++ MobileInputLessons/Assets/Scripts/DebugRayTracingLine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugRayTracingLine : MonoBehaviour
+{
+    // Frame update example: Draws a 100 meter long green line from the position for 1 frame.
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 100;
+        Debug.DrawRay(transform.position, forward, Color.green);
+    }
+}
