49389e824fac749baf272a54e91504c23c73ac43
DESKTOP-1HOV35S\aljon <>
2020-12-15 21:32:42 +0800

    edit

--- MobileInputLessons/Assets/Scripts/Gameplay/PlayerControls.cs
+++ MobileInputLessons/Assets/Scripts/Gameplay/PlayerControls.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     public Joystick joystick;
+    public Transform crossHair;
     private float sensitivity = 1000.0f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(joystick.Horizontal * sensitivity * Time.deltaTime, joystick.Vertical * sensitivity * Time.deltaTime, 0);
+        aimCrossHair();
+    }
+
+    private void aimCrossHair()
+    {
+        crossHair.position += new Vector3(joystick.Horizontal * sensitivity * Time.deltaTime, joystick.Vertical * sensitivity * Time.deltaTime, 0);
+    }
+
+    public void Shoot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(crossHair.position);
+        RaycastHit enemyHit;
+        if (Physics.Raycast(ray, out enemyHit))
+        {
+            if (enemyHit.collider != null)
+            {
+                if (enemyHit.collider.tag == "Enemy")
+                {
+                    Debug.Log("3D Hit: " + enemyHit.collider.name);
+                    enemyHit.collider.gameObject.GetComponent<EnemyBehavior>().onGetHit();
+                }
+            }
+        }
     }
 }
