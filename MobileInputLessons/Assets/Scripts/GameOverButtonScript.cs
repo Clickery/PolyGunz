7b6e391fcc2ef7c4e95e00481ec4108f41cd3a45
Clickery <garret_villanueva@dlsu.edu.ph>
2020-12-15 20:09:02 +0800

    Added Gameover Panel and buttons, Added OnClick template script in Weapon Upgrade Button

---
+++ MobileInputLessons/Assets/Scripts/GameOverButtonScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverButtonScript : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public GameObject playAgainButton;
+    public GameObject mainMenuButton;
+
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+        playAgainButton.SetActive(false);
+        mainMenuButton.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        /*
+        if(gameover == true){
+            gameOverPanel.SetActive(true);
+            playAgainButton.SetActive(true);
+            mainMenuButton.SetActive(true);
+        } 
+        */
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- MobileInputLessons/Assets/Scripts/WeaponUpgradeButtonScript.cs
+++ MobileInputLessons/Assets/Scripts/WeaponUpgradeButtonScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class WeaponUpgradeButtonScript : MonoBehaviour
 {
     public GameObject weaponUpgradeButton;
+    public GameObject redGunButton;
+    public GameObject blueGunButton;
+    public GameObject yellowGunButton;
     public GameObject weaponUpgradePanel;
 
     void Start()
@@ -24,6 +27,24 @@ public class WeaponUpgradeButtonScript : MonoBehaviour
         {
             weaponUpgradePanel.SetActive(false);
         }
+    }
+
+    public void UpgradeRedGunDamage()
+    {
+        // increase red gun damage IF PLAYER HAS ENOUGH POINTS
+        Debug.Log("UPGRADED RED GUN DAMAGE!");
+    }
+
+    public void UpgradeBlueGunDamage()
+    {
+        // increase blue gun damage IF PLAYER HAS ENOUGH POINTS
+        Debug.Log("UPGRADED BLUE GUN DAMAGE!");
+    }
+
+    public void UpgradeYellowGunDamage()
+    {
+        // increase yellow gun damage IF PLAYER HAS ENOUGH POINTS
+        Debug.Log("UPGRADED YELLOW GUN DAMAGE!");
     }
 
 }
