f4689bacc7529c78f425df5b387fb325acb346d8
DESKTOP-1HOV35S\aljon <>
2020-12-15 15:16:22 +0800

    Added crosshair image, and crosshairmovement script

---
+++ MobileInputLessons/Assets/Scripts/Gameplay/PlayerControls.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public Joystick joystick;
+    private float sensitivity = 1000.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(joystick.Horizontal * sensitivity * Time.deltaTime, joystick.Vertical * sensitivity * Time.deltaTime, 0);
+    }
+}
