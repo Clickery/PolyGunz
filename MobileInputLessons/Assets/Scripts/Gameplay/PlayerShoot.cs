e179d0909948686a8c33ca8134b1dfc56b1b0874
DESKTOP-1HOV35S\aljon <>
2020-12-15 18:38:24 +0800

    Added basic controls and logics

---
+++ MobileInputLessons/Assets/Scripts/Gameplay/PlayerShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public Transform crosshairPos;
+
+
+    public void Shoot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(crosshairPos.position);
+        RaycastHit enemyHit;
+        if(Physics.Raycast(ray, out enemyHit))
+        {
+            if(enemyHit.collider != null)
+            {
+                if(enemyHit.collider.tag == "Enemy")
+                {
+                    Debug.Log("3D Hit: " + enemyHit.collider.name);
+                    enemyHit.collider.gameObject.GetComponent<EnemyBehavior>().onGetHit();
+                }
+            }
+        }
+    }
+}
